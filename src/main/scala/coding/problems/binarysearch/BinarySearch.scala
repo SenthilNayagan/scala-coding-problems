38e248e80ade2106395bf2086dc34578e502428c
Senthil Nayagan <hello@senthilnayagan.com>
2022-10-12 20:17:58 +0530

    Added tail recursion approach

--- src/main/scala/coding/problems/binarysearch/BinarySearch.scala
+++ src/main/scala/coding/problems/binarysearch/BinarySearch.scala
@@ -2,13 +2,17 @@ package coding.problems.binarysearch
 
 import coding.problems.arrays.ArraysExercises.numbers
 
+import scala.annotation.tailrec
+
 class BinarySearch {
 
   /**
-   * Performing binary search using recursive approach.
+   * Performing binary search using non-tail-recursion approach.
+   * The stack depth (size) is more for Non-tail-recursive methods. Before doing the actual calculation, each
+   * recursive call must be finished.
    *
    * Time complexity: O(log n)
-   * Space complexity: O(1)
+   * Space complexity: O(n) - Because with every call to the recursive function, the state is saved on the call stack.
    *
    * @param nums list of integer numbers
    * @param searchItem element to be searched
@@ -16,7 +20,7 @@ class BinarySearch {
    * @param endIndex ending index of the list
    * @return index of the searchItem, if found. Otherwise, returns -1
    */
-  def binarySearchUsingRecursive(nums: List[Int], searchItem: Int)
+  def binarySearchUsingRecursion(nums: List[Int], searchItem: Int)
                                 (startIndex: Int = 0, endIndex: Int = nums.length -1): Int = {
     if (startIndex > endIndex) -1
 
@@ -25,22 +29,65 @@ class BinarySearch {
 
     if (nums(midIndex) == searchItem) midIndex
     else if (nums(midIndex) > searchItem)
-      binarySearchUsingRecursive(nums, searchItem)(startIndex, midIndex - 1)
+      binarySearchUsingRecursion(nums, searchItem)(startIndex, midIndex - 1)
     else
-      binarySearchUsingRecursive(nums, searchItem)(midIndex + 1, endIndex)
+      binarySearchUsingRecursion(nums, searchItem)(midIndex + 1, endIndex)
   }
 
+  /**
+   * Performing binary search using tail-recursion approach. The tail recursive functions better than non tail
+   * recursive functions because tail-recursion can be optimized by compiler. A recursive function is said to be
+   * tail recursive if the recursive call is the last operation performed by the function. There is no need to
+   * keep record of the previous state.
+   *
+   * For tail recursion function the following package to imported: scala.annotation.tailrec
+   *
+   * @param nums       list of integer numbers
+   * @param searchItem element to be searched
+   * @return index of the searchItem, if found. Otherwise, returns -1
+   */
+  def binarySearchUsingTailRecursion(nums: List[Int], searchItem: Int): Int = {
+    /**
+     *
+     * @param nums list of integer numbers
+     * @param searchItem element to be searched
+     * @param startIndex starting index of the list
+     * @param endIndex ending index of the list
+     * @return index of the searchItem, if found. Otherwise, returns -1
+     */
+    @tailrec def bsHelper(nums: List[Int], searchItem: Int, startIndex: Int, endIndex: Int): Int = {
+      if (startIndex > endIndex) -1
+
+      // Finding middle index
+      val midIndex = startIndex + (endIndex - startIndex) / 2
+
+      nums(midIndex) match {
+        case i if (i == searchItem) => midIndex
+        case i if (i > searchItem) => bsHelper(nums, searchItem, startIndex, midIndex - 1)
+        case _ => bsHelper(nums, searchItem, midIndex + 1, endIndex)
+      }
+    }
+    // Calling the helper
+    bsHelper(nums, searchItem, 0, nums.length - 1)
+  }
 
 }
 
 object BinarySearch extends App {
   val numbers: List[Int] = List(5, 2, 8, 7, 3, 1, 9, 4, 6).sorted
-  val searchItem = 7
+  val searchItem1 = 7
+  val searchItem2 = 3
 
   val binarySearch = new BinarySearch
 
-  println(binarySearch.binarySearchUsingRecursive(numbers, searchItem)() match {
-    case -1 => s"$searchItem doesn't exist"
-    case index => s"$searchItem exists at index $index"
+  println(binarySearch.binarySearchUsingRecursion(numbers, searchItem1)() match {
+    case -1 => s"$searchItem1 doesn't exist"
+    case index => s"$searchItem1 exists at index $index"
   })
+
+  println(binarySearch.binarySearchUsingTailRecursion(numbers, searchItem2) match {
+    case -1 => s"$searchItem2 doesn't exist"
+    case index => s"$searchItem2 exists at index $index"
+  })
+
 }
