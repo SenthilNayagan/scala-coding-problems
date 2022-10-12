1ad169b082709960ac5ac5fbf965e3de685fb44b
Senthil Nayagan <hello@senthilnayagan.com>
2022-10-12 17:35:04 +0530

    Initial commit

--- src/main/scala/coding/problems/arrays/ArraysExercises.scala
+++ src/main/scala/coding/problems/arrays/ArraysExercises.scala
@@ -1,5 +1,16 @@
 package coding.problems.arrays
 
+/**
+ * Array
+ * =====
+ * Array in scala is homogeneous and mutable.
+ *
+ *
+ * ArrayBuffer
+ * ==========
+ * Unlike Array, ArrayBuffer's size can change.
+ * To use ArrayBuffer, we need to import scala.collection.mutable.ArrayBuffer.
+ */
 object ArraysExercises extends App{
   // Declare array of integers
   var numbers: Array[Int] = Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
@@ -9,6 +20,9 @@ object ArraysExercises extends App{
 
   // Fetch array item by index
   print("\n"); println(numbers(2))
+
+  // Appending an element to int array
+  numbers :+ 10
 
   // Map functions
   numbers.map(_ * 2).foreach(print)  // Double each integer item in an array without the need of any variable.
@@ -21,6 +35,23 @@ object ArraysExercises extends App{
   print("\nAdding two arrays\n"); (numSet1 ++ numSet2).foreach(print)
 
   // Sorting an array
+  print("\n")
   val unsortedNums: Array[Int] = Array(7, 2, 9, 3, 1, 4, 0, 8)
-  print("\n"); unsortedNums.sorted.foreach(print)
+  unsortedNums.sorted.foreach(print)
+
+  // Range
+  val x = (1 to 5).toArray
+  print("\nArray of type integer created using range\n"); x.foreach(print)
+
+  // String arrays
+  val fruits: Array[String] = Array("Apple", "Orange", "Banana", "Mango")
+
+  // Check if an item exists
+  print("\n")
+  println(fruits.exists(_ == "Apple"))
+  println(fruits.exists(x => x == "Orange"))
+
+  // Appending string item to a string array
+  (fruits :+ "Papaya").foreach(print)  // "Papaya" won't be appended to the immutable fruits variable
+  print("\n"); fruits.foreach(print)
 }
